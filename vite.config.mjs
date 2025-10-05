3b4d1daf9391bb2083357ed3cf058d213bfbeda0
shineetejol9 <shineetejolpatro@gmail.com>
2025-10-05 22:42:49 +0530

    fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/zestverse1", // ✅ move it here
   plugins: [
     tailwindcss(),
-    react()
+    react(),
   ],
 })
